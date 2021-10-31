91b85757b451e7e103bfd64f639e7181e9df49da
Tim Sparg <timothy.sparg@gmail.com>
2021-10-31 22:03:37 +0100

    Changes to make vs code happy

--- .github/actions/whitelisting/jest.config.js
+++ .github/actions/whitelisting/jest.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
-}
+};
