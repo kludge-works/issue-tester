e3bcde4e832c1602bc571dfaf620fe91b207fd79
Tim Sparg <timothy.sparg@gmail.com>
2021-11-13 15:33:47 +0100

    Add work on retrieving issues from gh

--- .github/actions/whitelisting/__tests__/main.test.ts
+++ .github/actions/whitelisting/__tests__/main.test.ts
@@ -2,15 +2,17 @@ import { run } from "../src/issuer";
 import * as github from "@actions/github";
 import * as core from "@actions/core";
 
-const fs = jest.requireActual("fs");
-
 jest.mock("@actions/core");
 jest.mock("@actions/github");
 
 const gh = github.getOctokit("_");
-const addLabelsMock = jest.spyOn(gh.rest.issues, "addLabels");
+const errorSpy = jest.spyOn(core, "error");
+const issueSpy = jest.spyOn(gh.rest.issues, "get");
 
-afterAll(() => jest.restoreAllMocks());
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("run", () => {
 
@@ -19,12 +21,35 @@ describe("run", () => {
       "repo-token": "foo"
     };
 
+    github.context.payload.issue = undefined;
+
     jest
       .spyOn(core, "getInput")
       .mockImplementation((name: string, ...opts) => mockInput[name]);
 
     await run();
 
-    expect(addLabelsMock).toHaveBeenCalledTimes(0);
+    expect(errorSpy).toHaveBeenCalledTimes(1);
+    expect(errorSpy.mock.calls[0][0]).toBe("Could not get issue number from context, exiting")
+    expect(issueSpy).toHaveBeenCalledTimes(0);
+
+  });
+
+  it("green line", async () => {
+    let mockInput = {
+      "repo-token": "foo"
+    };
+
+    github.context.payload.issue = {number: 123}
+
+    jest
+        .spyOn(core, "getInput")
+        .mockImplementation((name: string, ...opts) => mockInput[name]);
+
+    await run();
+
+    expect(errorSpy).toHaveBeenCalledTimes(0);
+    expect(issueSpy).toHaveBeenCalledTimes(1);
+
   });
 });
--- .github/actions/whitelisting/src/issuer.ts
+++ .github/actions/whitelisting/src/issuer.ts
@@ -1,22 +1,31 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import * as Context from "@actions/github/lib/context";
 
 type ClientType = ReturnType<typeof github.getOctokit>;
 
 export async function run() {
   try {
     const token = core.getInput("repo-token", { required: true });
+    const issue_number = getIssueNumber(github.context);
 
-    const issueNumber = getIssueNumber();
-
-    const client: ClientType = github.getOctokit(token);
-
-    if (!issueNumber) {
+    if (!issue_number) {
       core.error("Could not get issue number from context, exiting");
       return;
     }
 
-    core.info(`issue number is ${issueNumber}`)
+    const client: ClientType = github.getOctokit(token);
+
+    core.info(`issue number is ${issue_number}`);
+
+    const { data: issue } = await client.rest.issues.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number,
+    })
+
+    core.info(JSON.stringify(issue));
+
 
   } catch (error: any) {
     core.error(error);
@@ -24,11 +33,12 @@ export async function run() {
   }
 }
 
-function getIssueNumber(): number | undefined {
-  const issue = github.context.payload.issue;
+function getIssueNumber(context: Context.Context): number | undefined {
+  const issue = context.payload.issue;
   if (!issue) {
     return undefined;
+  }else{
+    return issue.number;
   }
-
-  return issue.number;
 }
+
